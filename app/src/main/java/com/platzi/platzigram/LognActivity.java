8fa77659adc141288c69c92843f17358d03848fb
Claudia Villarroel Villarroel <Claudia Villarroel Villarroel>
2016-11-20 13:54:17 -0300

    Initial commit

---
+++ app/src/main/java/com/platzi/platzigram/LognActivity.java
@@ -0,0 +1,23 @@
+package com.platzi.platzigram;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.platzi.platzigram.view.CreateAccountActivity;
+
+public class LognActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logn);
+    }
+
+    public  void goCreateAccount(View view){
+
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
+}
